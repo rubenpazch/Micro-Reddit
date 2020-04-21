c8c847afa7b16b285fdf33f600dca2de648072e1
idgm5 <idgm@pm.me>
2020-04-21 16:01:43 -0500

    Add comment model and added relationship between posts and comments

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
   has_many :posts
+  has_many :comments
   validates :name, :presence => true, :uniqueness => {:scope => :name}
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_20_220050) do
+ActiveRecord::Schema.define(version: 2020_04_21_204906) do
+
+  create_table "comments", force: :cascade do |t|
+    t.text "body"
+    t.integer "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.string "post_type"
+    t.integer "post_id"
+    t.index ["post_type", "post_id"], name: "index_comments_on_post_type_and_post_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
@@ -27,5 +38,6 @@ ActiveRecord::Schema.define(version: 2020_04_20_220050) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "comments", "users"
   add_foreign_key "posts", "users"
 end
